fe0b6c12d6fa47a02791485602f5ca03f676295a
Nikita Zlobin <zlobin.n.s@yandex.ru>
2024-12-15 21:21:09 +0700

    minor edits have been made to the news details screen

--- lib/screens/news_detail_screen.dart
+++ lib/screens/news_detail_screen.dart
@@ -9,32 +9,50 @@ class NewsDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final article = context.read<NewsCubit>().selectedArticle;
 
+    final Color primaryColor = const Color(0xFFF6F1EC);
+    final Color blockColor = const Color(0xFFFFFFFF);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Детали новости'),
+        backgroundColor: blockColor,
+        title: const Text(
+          'Детали новости',
+          style: TextStyle(color: Colors.black),
+        ),
+        iconTheme: const IconThemeData(color: Colors.black),
+        elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                article?.title ?? 'Без заголовка',
-                style: const TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
+      body: Container(
+        color: primaryColor,
+        width: double.infinity,
+        height: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  article?.title ?? 'Без заголовка',
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              if (article?.urlToImage != null)
-                Image.network(article!.urlToImage!),
-              const SizedBox(height: 20),
-              Text(
-                article?.description ?? 'Описание отсутствует.',
-                style: const TextStyle(fontSize: 16),
-              ),
-            ],
+                const SizedBox(height: 10),
+                if (article?.urlToImage != null)
+                  Image.network(
+                    article!.urlToImage!,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                const SizedBox(height: 10),
+                Text(
+                  article?.description ?? 'Описание отсутствует.',
+                  style: const TextStyle(fontSize: 16),
+                )
+              ],
+            ),
           ),
         ),
       ),
